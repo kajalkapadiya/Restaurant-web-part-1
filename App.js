12ed7c03aeb3b8d8523e7e1036afaeff5ce5ccc1
kajalkapadiya <kajalkapadiya18@gmail.com>
2023-02-23 00:06:09 +0530

    restaurant website part-1

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useState } from "react";
+import Cart from "./Component/Cart/Cart";
+import Header from "./Component/Layout/Header";
+import Meals from "./Component/Meals/Meals";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <Fragment>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
